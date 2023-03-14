dd1dfa580ec7ca9692ad75c2dcd3c7b0e8074a37
PrototypeCodec <kcodef27@gmail.com>
2023-03-14 20:02:36 +0000

    Basic function vanne call

---
+++ app.js
@@ -0,0 +1,127 @@
+/*
+ * Copyright IBM Corp. All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const { Gateway, Wallets } = require('fabric-network');
+const FabricCAServices = require('fabric-ca-client');
+const path = require('path');
+const os = require("os");
+const { buildCAClient, registerAndEnrollUser, enrollAdmin } = require(os.homedir() + '/ValveChain-Application/test-application/CAUtil.js');
+const { buildCCPOrg1, buildWallet } = require(os.homedir() + '/ValveChain-Application/test-application/AppUtil.js');
+
+const channelName = process.env.CHANNEL_NAME || 'mychannel';
+const chaincodeName = process.env.CHAINCODE_NAME || 'basic';
+
+const mspOrg1 = 'Org1MSP';
+const walletPath = path.join(__dirname, 'wallet');
+const org1UserId = 'javascriptAppUser';
+
+function prettyJSONString(inputString) {
+    return JSON.stringify(JSON.parse(inputString), null, 2);
+}
+
+// pre-requisites:
+// - fabric-sample two organization test-network setup with two peers, ordering service,
+//   and 2 certificate authorities
+//         ===> from directory /fabric-samples/test-network
+//         ./network.sh up createChannel -ca
+// - Use any of the asset-transfer-basic chaincodes deployed on the channel "mychannel"
+//   with the chaincode name of "basic". The following deploy command will package,
+//   install, approve, and commit the javascript chaincode, all the actions it takes
+//   to deploy a chaincode to a channel.
+//         ===> from directory /fabric-samples/test-network
+//         ./network.sh deployCC -ccn basic -ccp ~/ValveChain-chaincode/ -ccl javascript
+// - Be sure that node.js is installed
+//         ===> from directory /ValveChain-Application
+//         node -v
+// - npm installed code dependencies
+//         ===> from directory /ValveChain-Application
+//         npm install
+// - to run this test application
+//         ===> from directory /ValveChain-Application
+//         node app.js
+
+// NOTE: If you see  kind an error like these:
+/*
+    2020-08-07T20:23:17.590Z - error: [DiscoveryService]: send[mychannel] - Channel:mychannel received discovery error:access denied
+    ******** FAILED to run the application: Error: DiscoveryService: mychannel error: access denied
+   OR
+   Failed to register user : Error: fabric-ca request register failed with errors [[ { code: 20, message: 'Authentication failure' } ]]
+   ******** FAILED to run the application: Error: Identity not found in wallet: appUser
+*/
+// Delete the /ValveChain-Application/wallet directory
+// and retry this application.
+//
+// The certificate authority must have been restarted and the saved certificates for the
+// admin and application user are not valid. Deleting the wallet store will force these to be reset
+// with the new certificate authority.
+//
+
+async function main() {
+    try {
+        // build an in memory object with the network configuration (also known as a connection profile)
+        const ccp = buildCCPOrg1();
+
+        // build an instance of the fabric ca services client based on
+        // the information in the network configuration
+        const caClient = buildCAClient(FabricCAServices, ccp, 'ca.org1.example.com');
+
+        // setup the wallet to hold the credentials of the application user
+        const wallet = await buildWallet(Wallets, walletPath);
+
+        // in a real application this would be done on an administrative flow, and only once
+        await enrollAdmin(caClient, wallet, mspOrg1);
+
+        // in a real application this would be done only when a new user was required to be added
+        // and would be part of an administrative flow
+        await registerAndEnrollUser(caClient, wallet, mspOrg1, org1UserId, 'org1.department1');
+
+        // Create a new gateway instance for interacting with the fabric network.
+        // In a real application this would be done as the backend server session is setup for
+        // a user that has been verified.
+        const gateway = new Gateway();
+
+        try {
+            // setup the gateway instance
+            // The user will now be able to create connections to the fabric network and be able to
+            // submit transactions and query. All transactions submitted by this gateway will be
+            // signed by this user using the credentials stored in the wallet.
+            await gateway.connect(ccp, {
+                wallet,
+                identity: org1UserId,
+                discovery: { enabled: true, asLocalhost: true } // using asLocalhost as this gateway is using a fabric network deployed locally
+            });
+
+            // Build a network instance based on the channel where the smart contract is deployed
+            const network = await gateway.getNetwork(channelName);
+
+            // Get the contract from the network.
+            const contract = network.getContract(chaincodeName);
+
+            await contract.submitTransaction('createVanne', 'vanne1', 'Vanne 1', 'Description de la Vanne 1', '1', '2', '30', '25', '30');
+            const result = await contract.evaluateTransaction('getVanneById', 'vanne1');
+            console.log('Transaction returned: ' + result.toString());
+
+            console.log("==================================")
+
+            const allResults = await contract.evaluateTransaction('getAllVannes');
+            console.log('All vannes: ' + allResults.toString());
+
+
+        } finally {
+            // Disconnect from the gateway when the application is closing
+            // This will close all connections to the network
+            gateway.disconnect();
+        }
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+}
+
+
+main();
